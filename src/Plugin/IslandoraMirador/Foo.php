9d9737d387041f3dd491bba093634f83203bc3d0
Alexander O'Neill <aloneill@gmail.com>
2022-09-09 19:30:25 +0000

    Add generated plugin manager files.

---
+++ src/Plugin/IslandoraMirador/Foo.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drupal\islandora_mirador\Plugin\IslandoraMirador;
+
+use Drupal\islandora_mirador\IslandoraMiradorPluginBase;
+
+/**
+ * Plugin implementation of the islandora_mirador.
+ *
+ * @IslandoraMirador(
+ *   id = "foo",
+ *   label = @Translation("Foo"),
+ *   description = @Translation("Foo description.")
+ * )
+ */
+class Foo extends IslandoraMiradorPluginBase {
+
+}
