58ce7537c0fa56585be0c668ede17aaca2885f95
Alexander O'Neill <aloneill@gmail.com>
2022-09-09 19:42:32 +0000

    Support for Text Overlay plugin.

--- js/mirador_viewer.js
+++ js/mirador_viewer.js
@@ -5,49 +5,65 @@
  * Displays Mirador viewer.
  */
 (function ($, Drupal) {
-    'use strict';
+  'use strict';
 
-    /**
-     * If initialized.
-     * @type {boolean}
-     */
-    var initialized;
-    /**
-     * Unique HTML id.
-     * @type {string}
-     */
-    var base;
+  /**
+   * If initialized.
+   * @type {boolean}
+   */
+  var initialized;
+  /**
+   * Unique HTML id.
+   * @type {string}
+   */
+  var base;
 
-    function init(context,settings){
-        if (!initialized){
-            initialized = true;
-            var miradorInstance = Mirador.viewer({
-                "id": base,
-                "manifests": {
-                    [settings.iiif_manifest_url]: {provider: "Islandora"}
-                },
-                "windows": [
-                    {
-                        "manifestId": settings.iiif_manifest_url,
-                        "thumbnailNavigationPosition": 'far-bottom'
-                    }
-                ]
-            })
-
+  function init(context,settings){
+    if (!initialized){
+      initialized = true;
+      let plugins = [];
+      if (window.miradorPlugins && window.miradorPlugins.length) {
+        for (let { name, plugin } of window.miradorPlugins) {
+          plugins = [...plugins, ...plugin];
         }
-    }
-    Drupal.Mirador = Drupal.Mirador || {};
+      }
 
-    /**
-     * Initialize the Mirador Viewer.
-     */
-    Drupal.behaviors.Mirador = {
-        attach: function (context, settings) {
-            base = settings.mirador_view_id;
-            init(context,settings);
+      var miradorInstance = Mirador.viewer({
+        "id": base,
+        "manifests": {
+          [settings.iiif_manifest_url]: {provider: "Islandora"}
         },
-        detach: function () {
-        }
-    };
+        "window": {
+          "textOverlay": {
+            enabled: true,
+            selectable: true,
+            visible: true,
+          }
+        },
+        "windows": [
+          {
+            "manifestId": settings.iiif_manifest_url,
+            "thumbnailNavigationPosition": 'far-bottom'
+
+
+          }
+        ]
+      }, plugins);
+
+    }
+  }
+  Drupal.Mirador = Drupal.Mirador || {};
+
+  /**
+   * Initialize the Mirador Viewer.
+   */
+  Drupal.behaviors.Mirador = {
+    attach: function (context, settings) {
+      base = settings.mirador_view_id;
+      init(context,settings);
+    },
+    detach: function () {
+    }
+  };
 
 })(jQuery, Drupal);
