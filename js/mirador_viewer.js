c2e9e6dbc7c6f7254e56ed1f283aebcb20bf1848
Alexander O'Neill <aloneill@gmail.com>
2022-09-22 20:05:12 +0000

    WIP UPdate how Mirador is instantiated.

--- js/mirador_viewer.js
+++ js/mirador_viewer.js
@@ -1,69 +1,55 @@
 /*jslint browser: true*/
-/*global Mirador, Drupal*/
+/*global Mirador, textOverlayPlugin, Drupal*/
 /**
  * @file
  * Displays Mirador viewer.
  */
 (function ($, Drupal) {
-  'use strict';
+    'use strict';
 
-  /**
-   * If initialized.
-   * @type {boolean}
-   */
-  var initialized;
-  /**
-   * Unique HTML id.
-   * @type {string}
-   */
-  var base;
+    /**
+     * If initialized.
+     * @type {boolean}
+     */
+    var initialized;
+    /**
+     * Unique HTML id.
+     * @type {string}
+     */
+    var base;
 
-  function init(context,settings){
-    if (!initialized){
-      initialized = true;
-      let plugins = [];
-      if (window.miradorPlugins && window.miradorPlugins.length) {
-        for (let { name, plugin } of window.miradorPlugins) {
-          plugins = [...plugins, ...plugin];
+    function init(context,settings){
+        if (!initialized){
+            initialized = true;
+            var miradorInstance = Mirador.viewer({
+                "id": base,
+                "manifests": {
+                    [settings.iiif_manifest_url]: {provider: "Islandora"}
+                },
+                "windows": [
+                    {
+                        "manifestId": settings.iiif_manifest_url,
+                        "thumbnailNavigationPosition": 'far-bottom'
+                    }
+                ]
+            },  [
+        ...textOverlayPlugin,
+        ]);
+
         }
-      }
-
-      var miradorInstance = Mirador.viewer({
-        "id": base,
-        "manifests": {
-          [settings.iiif_manifest_url]: {provider: "Islandora"}
-        },
-        "window": {
-          "textOverlay": {
-            enabled: true,
-            selectable: true,
-            visible: true,
-          }
-        },
-        "windows": [
-          {
-            "manifestId": settings.iiif_manifest_url,
-            "thumbnailNavigationPosition": 'far-bottom'
-
-
-          }
-        ]
-      }, plugins);
-
     }
-  }
-  Drupal.Mirador = Drupal.Mirador || {};
+    Drupal.Mirador = Drupal.Mirador || {};
 
-  /**
-   * Initialize the Mirador Viewer.
-   */
-  Drupal.behaviors.Mirador = {
-    attach: function (context, settings) {
-      base = settings.mirador_view_id;
-      init(context,settings);
-    },
-    detach: function () {
-    }
-  };
+    /**
+     * Initialize the Mirador Viewer.
+     */
+    Drupal.behaviors.Mirador = {
+        attach: function (context, settings) {
+            base = settings.mirador_view_id;
+            init(context,settings);
+        },
+        detach: function () {
+        }
+    };
 
 })(jQuery, Drupal);
