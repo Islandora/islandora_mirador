2dad97728b7b0e8c0edd617bc5b9d7e046162d87
Joe Corall <jjc223@lehigh.edu>
2026-03-05 04:55:22 -0500

    Allow changing the theme and primary/secondary colors

--- src/Form/MiradorConfigForm.php
+++ src/Form/MiradorConfigForm.php
@@ -72,6 +72,51 @@ class MiradorConfigForm extends ConfigFormBase {
       '#options' => $plugins,
       '#default_value' => $config->get('mirador_enabled_plugins'),
     ];
+    $form['mirador_theme_fieldset'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Theme'),
+      '#description' => $this->t('Configure the Mirador viewer theme and color palette. See the <a href=":url">Mirador theming documentation</a> for more information.', [
+        ':url' => 'https://github.com/ProjectMirador/mirador/wiki/M3-Theming-Mirador',
+      ]),
+    ];
+    $form['mirador_theme_fieldset']['mirador_selected_theme'] = [
+      '#type' => 'radios',
+      '#title' => $this->t('Selected theme'),
+      '#options' => [
+        'light' => $this->t('Light'),
+        'dark' => $this->t('Dark'),
+      ],
+      '#default_value' => $config->get('mirador_selected_theme'),
+    ];
+    $form['mirador_theme_fieldset']['mirador_theme_light'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Light theme colors'),
+    ];
+    $form['mirador_theme_fieldset']['mirador_theme_light']['mirador_theme_light_primary'] = [
+      '#type' => 'color',
+      '#title' => $this->t('Primary color'),
+      '#default_value' => $config->get('mirador_theme_light_primary'),
+    ];
+    $form['mirador_theme_fieldset']['mirador_theme_light']['mirador_theme_light_secondary'] = [
+      '#type' => 'color',
+      '#title' => $this->t('Secondary color'),
+      '#default_value' => $config->get('mirador_theme_light_secondary'),
+    ];
+    $form['mirador_theme_fieldset']['mirador_theme_dark'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Dark theme colors'),
+    ];
+    $form['mirador_theme_fieldset']['mirador_theme_dark']['mirador_theme_dark_primary'] = [
+      '#type' => 'color',
+      '#title' => $this->t('Primary color'),
+      '#default_value' => $config->get('mirador_theme_dark_primary'),
+    ];
+    $form['mirador_theme_fieldset']['mirador_theme_dark']['mirador_theme_dark_secondary'] = [
+      '#type' => 'color',
+      '#title' => $this->t('Secondary color'),
+      '#default_value' => $config->get('mirador_theme_dark_secondary'),
+    ];
+
     $form['iiif_manifest_url_fieldset'] = [
       '#type' => 'fieldset',
       '#title' => $this->t('IIIF Manifest URL'),
@@ -98,12 +143,42 @@ class MiradorConfigForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+
+    $theme = $form_state->getValue('mirador_selected_theme');
+    if (!in_array($theme, ['light', 'dark'], TRUE)) {
+      $form_state->setErrorByName('mirador_selected_theme', $this->t('Theme must be either "light" or "dark".'));
+    }
+
+    $color_fields = [
+      'mirador_theme_light_primary',
+      'mirador_theme_light_secondary',
+      'mirador_theme_dark_primary',
+      'mirador_theme_dark_secondary',
+    ];
+    foreach ($color_fields as $field) {
+      $value = $form_state->getValue($field);
+      if ($value !== NULL && $value !== '' && !preg_match('/^#[0-9a-fA-F]{3}([0-9a-fA-F]{3})?$/', $value)) {
+        $form_state->setErrorByName($field, $this->t('Color must be a valid CSS hex color (e.g. #fff or #1967d2).'));
+      }
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $config = $this->config('islandora_mirador.settings');
     $config->set('mirador_library_installation_type', $form_state->getValue('mirador_library_installation_type'));
     $config->set('mirador_enabled_plugins', $form_state->getValue('mirador_enabled_plugins'));
     $config->set('iiif_manifest_url', $form_state->getValue('iiif_manifest_url'));
     $config->set('mirador_library_minified', $form_state->getValue('mirador_library_minified'));
+    $config->set('mirador_selected_theme', $form_state->getValue('mirador_selected_theme'));
+    $config->set('mirador_theme_light_primary', $form_state->getValue('mirador_theme_light_primary'));
+    $config->set('mirador_theme_light_secondary', $form_state->getValue('mirador_theme_light_secondary'));
+    $config->set('mirador_theme_dark_primary', $form_state->getValue('mirador_theme_dark_primary'));
+    $config->set('mirador_theme_dark_secondary', $form_state->getValue('mirador_theme_dark_secondary'));
     $config->save();
     parent::submitForm($form, $form_state);
   }
